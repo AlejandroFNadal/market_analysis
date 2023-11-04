9bbcf586cdc39cb9a624da152d072efaeda2286c
AlejandroFNadal <alexandronadal@gmail.com>
2023-11-04 15:49:29 +0100

    fix: lambda handler has proper parameters for a lambda handler. error logging improved

--- back/producer/producer.py
+++ back/producer/producer.py
@@ -8,6 +8,8 @@ from loguru import logger
 import os
 from dotenv import load_dotenv
 
+# create logger with 'DEBUG' level
+logger.add(sys.stderr, format="{time} {level} {message}", filter="my_module", level="DEBUG")
 
 load_dotenv()
 # Replace with your Etherscan API Key
@@ -53,21 +55,25 @@ def get_eth_price(api_key):
 def get_ec2_ip_by_tag(tag_name, tag_value):
     """Get the public IP of an EC2 instance by tag."""
     ec2_client = boto3.client('ec2')
-    response = ec2_client.describe_instances(Filters=[{'Name': f'tag:{tag_name}', 'Values': [tag_value]}])
+    # do not get terminated instances
+    response = ec2_client.describe_instances(Filters=[{'Name': f'tag:{tag_name}', 'Values': [tag_value]},
+                                                      {'Name': 'instance-state-name', 'Values': ['running']}])
     # Assuming only one instance with the given tag
     instance = response['Reservations'][0]['Instances'][0]
+    logger.info(f"Found EC2 instance: {instance}")
+    logger.info(f'Public IP of EC2 instance with tag {tag_name}={tag_value}: {instance["PublicIpAddress"]}')
     return instance['PublicIpAddress']
 
 # If the following variable is true, we need to try to reach the kafka server on aws. 
 # Otherwise, asume it is local
 
 
-if os.environ['KAFKA_SERVER_AWS'] == 'True':
+if os.environ['KAFKA_SERVER_AWS'] == 'true':
     ip = get_ec2_ip_by_tag('Name', 'KafkaAirflowServer')
 else:
     ip = 'localhost'
 conf = {
-    'bootstrap.servers': f'{ip}:9092',  # replace with your Kafka broker(s)
+    'bootstrap.servers': f'{ip}:9092',  # Kafka broker 
 }
 try:
     producer = Producer(conf)
@@ -77,26 +83,31 @@ except Exception as e:
 
 
 def main():
-    """Infinite loop to continuously fetch data from Etherscan."""
+    """Fetch data from Etherscan."""
     try:
         latest_block_number = get_latest_block_number(api_key)
         # check what is the last block number in dynamoDB, it is a single value
         # if the latest_block_number is greater than the last block number in dynamoDB, then 
         # we need to fetch the transaction from the last block number in dynamoDB to the latest_block_number
         boto3.setup_default_session(region_name='us-east-1')
-        if os.environ['DYNAMODB_SERVER_AWS'] == 'True':
-            dynamodb = boto3.resource('dynamodb')
-        else:
-            dynamodb = boto3.resource('dynamodb', endpoint_url="http://localhost:8000")
-        table = dynamodb.Table('BlockConfig')
-        # if we dont find it, just accept the latest_block_number as the last_processed_block
-        response = table.get_item(
-            Key={
-                'config_type': 'last_processed_block'
-            }
-        )
+        try:
+            if os.environ['DYNAMODB_SERVER_AWS'] == 'True':
+                dynamodb = boto3.resource('dynamodb')
+            else:
+                dynamodb = boto3.resource('dynamodb', endpoint_url="http://localhost:8000")
+            table = dynamodb.Table('BlockConfig')
+            # if we dont find it, just accept the latest_block_number as the last_processed_block
+            response = table.get_item(
+                Key={
+                    'config_type': 'last_processed_block'
+                }
+            )
+        except Exception as e:
+            logger.error(f'Failed to connect to DynamoDB: {e}')
+            raise Exception(f'Failed to connect to DynamoDB: {e}')
         try:
             last_processed_block = response['Item']['last_block_number']
+            logger.info(f"Last processed block: {last_processed_block}")
         except KeyError:
             # this is the first execution, no data in dynamoDB
             logger.info('No data in dynamoDB, setting last_processed_block to latest_block_number - 10')
@@ -106,8 +117,10 @@ def main():
         start_time = time.time()
         while last_processed_block < (latest_block_number - 10) and (time.time() - start_time) < 300:
             block_to_process = last_processed_block + 1
+            logger.info(f'Processing block number: {block_to_process}')
             transactions, timestamp = get_block_transactions(block_to_process, api_key)
             usd_price = get_eth_price(api_key)
+            logger.info(f'Current ETH price in USD: {usd_price}')
             filtered_transactions = []
             for tx in transactions:
                 val = wei_to_eth(int(tx["value"], 16))
@@ -130,33 +143,42 @@ def main():
             # create byte from bytearray
             message_bytes = bytes(json.dumps(object_to_send).encode('utf-8'))
             # write the filtered_transactions to kafka
-            producer.produce('eth', key=batch_key, value=message_bytes)
-            producer.flush()
-            print(f'Block Number: {block_to_process}, Transactions: {len(filtered_transactions)}')
+            try:
+                logger.debug(f"Sending message to Kafka")
+                producer.produce('eth', key=batch_key, value=message_bytes)
+                producer.flush()
+            except Exception as e:
+                logger.error(f'Failed to send message to Kafka: {e}')
+                raise Exception(f'Failed to send message to Kafka: {e}')
+            logger.info(f'Block Number: {block_to_process}, Transactions: {len(filtered_transactions)}')
             # update the last_processed_block in dynamoDB
-            table.update_item(
-                Key={
-                    'config_type': 'last_processed_block'
-                },
-                UpdateExpression='SET last_block_number = :val1',
-                ExpressionAttributeValues={
-                    ':val1': block_to_process
-                }
-            )
+            try:
+                table.update_item(
+                    Key={
+                        'config_type': 'last_processed_block'
+                    },
+                    UpdateExpression='SET last_block_number = :val1',
+                    ExpressionAttributeValues={
+                        ':val1': block_to_process
+                    }
+                )
+            except Exception as e:
+                logger.error(f'Failed to update last_processed_block in dynamoDB: {e}')
             last_processed_block = block_to_process
     except Exception as e:
-        logger.error(f'Failed to fetch data from Etherscan: {e}')
+        logger.error(f'Sth has failed - : {e}')
 
 
-def handler():
+def handler(event, context):
     """Lambda handler function."""
     return main()
 
 
 if __name__ == "__main__":
+    logger.warning("Running locally")
     # lambda simulation, every 30 seconds
     while True:
         main()
-        time.sleep(2)
+        time.sleep(30)
 
 
