37f4c6fd71cb40c1049abbb024e9a04b33f3e4c7
AlejandroFNadal <alexandronadal@gmail.com>
2023-11-22 21:33:25 +0100

    consumer logs info about how many pending messages to read in its partition still has to go

--- back/consumer/consumer.py
+++ back/consumer/consumer.py
@@ -9,6 +9,7 @@ from utils import wei_to_eth
 import os
 from dotenv import load_dotenv
 import sys
+import traceback
 
 logger.remove()
 logger.add(sys.stdout, level='DEBUG')
@@ -50,6 +51,9 @@ def save_to_s3(timestamp, batch_key, avg_gas, usd_price):
 
 def process_batch(batch_key, block):
     transactions = block['transactions']
+    if len(transactions) == 0:
+        logger.warning(f"Empty batch with key {batch_key}")
+        return
     gas_values = [tx["gas"] for tx in transactions]
     avg_gas = average(gas_values)
     usd_price = wei_to_eth(avg_gas) * transactions[0]["usd_price"]
@@ -79,44 +83,71 @@ def get_ec2_ip_by_tag(tag_name, tag_value):
     raise Exception(f"Could not find EC2 instance with tag {tag_name}={tag_value}")
 
 
-if os.environ['KAFKA_SERVER_AWS'] == 'true':
-    ip = get_ec2_ip_by_tag('Name', 'KafkaAirflowServer')
-else:
-    ip = 'localhost'
-conf = {
-    'bootstrap.servers': f'{ip}:9092',
-    'group.id': 'eth_consumer_group',
-    'auto.offset.reset': 'earliest'
-}
-try:
-    logger.debug("Creating consumer...")
-    consumer = Consumer(conf)
-    consumer.subscribe(['eth'])
-except Exception as e:
-    logger.error(f"Error creating consumer: {e}")
+def setup_consumer():
+    if os.environ['KAFKA_SERVER_AWS'] == 'true':
+        ip = get_ec2_ip_by_tag('Name', 'KafkaAirflowServer')
+    else:
+        ip = 'localhost'
+    conf = {
+        'bootstrap.servers': f'{ip}:9092',
+        'group.id': 'eth_consumer_group',
+        'auto.offset.reset': 'earliest'
+    }
+    try:
+        logger.debug("Creating consumer...")
+        consumer = Consumer(conf)
+        consumer.subscribe(['eth'])
+        return consumer, ip
+    except Exception as e:
+        logger.error(f"Error creating consumer: {e}")
+
+
+consumer, ip = setup_consumer()
 
 
 def main():
     """Get message from Kafka, calculate stats, save to S3."""
+    global consumer, ip
     try:
         # run under 30 seconds to avoid Lambda timeout
         start_time = datetime.now()
-        while (datetime.now() - start_time).seconds < 30:
-            message = consumer.poll(2.0)
+        messages = consumer.consume(10, 2.0)
+        consumer_asignment = consumer.assignment()
+        while len(consumer_asignment) != 0 and (datetime.now() - start_time).seconds < 30:
+            if len(messages) == 0:
+                message = None
+            else:
+                message = messages.pop(0)
+            # this gives the partition to which the consumer was assigned
+            logger.debug(f"Consumer assignment: {consumer_asignment}")
             if message is None:
                 logger.debug("No message received")
             elif message.error():
                 logger.error(f"Error in message consumption: {message.error()}")
             else:
+                if len(consumer_asignment) != 0:
+                    # this gives the offset of the last message in the partition
+                    last_message_offset = consumer.get_watermark_offsets(consumer_asignment[0])[1]
+                    logger.debug(f"Last message offset: {last_message_offset}")
+                    pending_messages = last_message_offset - message.offset()
+                    logger.debug(f"Pending messages: {pending_messages}")
+                else:
+                    logger.debug("No consumer assignment")
                 insertion_timestamp = datetime.fromtimestamp(message.timestamp()[1] / 1000.0)
+                # Calculate pending messages in Kafka:
                 logger.info(f"Consumed message with insertion time {insertion_timestamp}")
                 block = json.loads(message.value().decode('utf-8'))
                 batch_key = message.key().decode('utf-8')
                 process_batch(batch_key, block)
+            if len(messages) == 0:
+                messages = consumer.consume(10, 2.0)
     except KeyboardInterrupt:
         logger.info("Consumer interrupted. Closing connection...")
+        consumer.close()
+        return 0
     except Exception as e:
         logger.error(f"Error in message consumption: {e}")
+        traceback.print_exc()
         return -1
 
 
@@ -127,5 +158,6 @@ def handler(event, context):
 
 if __name__ == "__main__":
     while True:
-        main()
+        if main() == 0:
+            break
 
